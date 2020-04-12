0a051061ce8dcf222973a5ba940dd69d77b0fac5
zhoulinhui <1047043261@qq.com>
2020-04-12 13:41:08 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/MusicVisualizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicVisualizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicVisualizationApplication.class, args);
+    }
+
+}
